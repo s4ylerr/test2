a649e9244fcc3d925dd9763ac2c369938853b39b
s4ylerr <mitko.stamboliyski@gmail.com>
2016-01-28 19:11:24 +0200

    28.01.16

---
+++ homeworks/task4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+ 	<?php 
+ 			<table border="1">
+			<tr>
+			<td></td>
+			<td></td>
+			</tr>
+			</table>
+			 ?>
+</body>
+</html>
